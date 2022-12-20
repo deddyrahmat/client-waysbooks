d16200b92ca6a8f5c5a588e1220ddede319de076
deddyrahmat <deddyrahmat97@gmail.com>
2022-12-20 07:15:28 +0700

    add redux persiste
    add package redux persist for persistent data

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,36 @@
 import {configureStore} from '@reduxjs/toolkit';
+import storage from "redux-persist/lib/storage";
+import { persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER, } from "redux-persist";
+import { combineReducers } from "redux";
+
 import authReducer from './authSlice';
 import cartReducer from './cartSlice';
 
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+  };
+  
+  const reducer = combineReducers({
+    authReducer,
+    cartReducer
+  });
+  
+  const persistedReducer = persistReducer(persistConfig, reducer);
+
 export const store = configureStore({
-    reducer : {
-        authReducer,
-        cartReducer
-    },
+    reducer : persistedReducer,
     devTools: process.env.NODE_ENV !== 'production',
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
